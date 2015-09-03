f37241d4cbe75a8e58b3690d2cc3510eeec559dc
Stephane Nicoll <snicoll@pivotal.io>
2015-09-03 17:26:14 +0200

    Manual meta-data demonstration (II)
    Spring Boot 1.3 introduced the notion of value hints. Let's specify that
    that counter can take two values (42 and 7) with a relevant description.
    
    Our `foo.counter` can only have the value of 42 and 7. What if these were
    only hints and any extra value is allowed. We can add a value provider
    for that.
    
    IJ provides auto-completion for all the available providers. More can be
    added in future release. Let's update our `foo.bar` to actually represent
    the name of a Spring Bean. Create two spring beans of whatever type, for
    instance String, and restrict the beans of String type in the context.

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,4 +10,15 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	public String one() {
+		return "one";
+	}
+
+	@Bean
+	public String two() {
+		return "two";
+	}
+
 }
